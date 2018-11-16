0ea19af295cf9ed6b40892b0bf9c214388ec78e3
= <=>
2018-11-16 23:48:58 +0530

    Added call and view website functionality

---
+++ Real Estate AR/Assets/BrandARManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrandARManager : MonoBehaviour
+{
+    public string phoneNumber;
+    public string websiteUrl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void callButton() {
+        Application.OpenURL("tel://" + phoneNumber);
+    }
+
+    public void websitelButton() {
+        Application.OpenURL(websiteUrl);
+    }
+}
